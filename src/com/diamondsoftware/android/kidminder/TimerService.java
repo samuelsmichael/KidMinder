f83ef8191dfc8f208175a7f08b65e7ab62e1fe8c
Mike Samuels <samuelsmichael222@gmail.com>
2014-08-22 05:49:19 -0600

    It works!  Play Station's Location Manager with interrupts was the trick.

--- src/com/diamondsoftware/android/kidminder/TimerService.java
+++ src/com/diamondsoftware/android/kidminder/TimerService.java
@@ -7,6 +7,8 @@ import java.util.TimerTask;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 
 import android.app.Service;
 import android.content.Context;
@@ -18,8 +20,8 @@ import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 
 public class TimerService extends Service implements GooglePlayServicesClient.ConnectionCallbacks,
-													 GooglePlayServicesClient.OnConnectionFailedListener {
-	private Timer mTimer=null;
+GooglePlayServicesClient.OnConnectionFailedListener,
+LocationListener  {
 	private SettingsManager mSettingsManager;
     private LocationClient mLocationClient;
     private boolean mImConnected2=false;
@@ -27,14 +29,53 @@ public class TimerService extends Service implements GooglePlayServicesClient.Co
 	private Location mPreviousLocation=null;
 	private Calendar mPreviousCalendar=null;
 	
+    boolean mUpdatesRequested;
+	
+    // Global constants
+    // Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    // Update frequency in seconds
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    // The fastest update frequency, in seconds
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+    // Define an object that holds accuracy and frequency parameters
+    LocationRequest mLocationRequest;
+	
 	private LocationManager getLocationManager() {
 		if (mLocationManager == null) {
 			mLocationManager = (android.location.LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		}
 		return mLocationManager;
 	}	
+    @Override
+    public void onLocationChanged(Location location) {
+		double speed=0;
+		Intent broadcastIntent2 = new Intent();
+        broadcastIntent2.setAction(GlobalStaticValues.HEARTBEAT_NOTIFICATION);
+        // Broadcast whichever result occurred
+        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent2);
+		
+		if(location.hasSpeed()) {
+			speed=(double)location.getSpeed();
+			speed=(speed*(double)3600)/1609.34;
+			Intent broadcastIntent = new Intent();
+	        broadcastIntent.setAction(GlobalStaticValues.GOTSPEED_NOTIFICATION);
+	        // Broadcast whichever result occurred
+	        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+		} 
+		Intent broadcastIntent = new Intent();
+	        broadcastIntent.setAction(GlobalStaticValues.SPEED_NOTIFICATION)
+	        	.putExtra("speed", speed);
+	        // Broadcast whichever result occurred
+	        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
 
-
+    }
 	private void doS(Location location) {
 		double speed=0;
 		
@@ -94,10 +135,8 @@ public class TimerService extends Service implements GooglePlayServicesClient.Co
 					reset();
 				} else {
 					if(action.equals(GlobalStaticValues.ACTION_HEARTBEAT_INTERVAL_CHANGED)) {
-						if(mTimer!=null) {
 							reset();
 							start();
-						}
 					}
 				}
 			} else {
@@ -110,18 +149,40 @@ public class TimerService extends Service implements GooglePlayServicesClient.Co
 	}		
 	private void reset() {
 		this.stopTimerHeartbeat();
-		if(mLocationClient!=null) {
-			mLocationClient.disconnect();
-			mLocationClient=null;
-		}
+        if (mLocationClient.isConnected()) {
+            /*
+             * Remove location updates for a listener.
+             * The current Activity is the listener, so
+             * the argument is "this".
+             */
+  //          this.getLocationManager().removeLocationUpdates(this);
+        }
+        /*
+         * After disconnect() is called, the client is
+         * considered "dead".
+         */
+        mLocationClient.disconnect();	
+		
 	}
 	private void start() {
 	    if (! getLocationManager().isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
 	    } else {
+	    	
+	        mLocationRequest = LocationRequest.create();
+	        // Use high accuracy
+	        mLocationRequest.setPriority(
+	                LocationRequest.PRIORITY_HIGH_ACCURACY);
+	        // Set the update interval to 5 seconds
+	        mLocationRequest.setInterval(UPDATE_INTERVAL);
+	        // Set the fastest update interval to 1 second
+	        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+	    	
+	    	
 			mSettingsManager=new SettingsManager(this);
-	        mLocationClient = new LocationClient(this, this, this);		
+	        mLocationClient = new LocationClient(this, this, this);
+	        // Start with updates turned off
+	        mUpdatesRequested = false;
 	        mLocationClient.connect();
-	        mPreviousLocation=null;
 	    }
 	}
 	@Override
@@ -130,40 +191,11 @@ public class TimerService extends Service implements GooglePlayServicesClient.Co
 	}
 	
 	private void stopTimerHeartbeat() {
-		if (mTimer != null) {
-			try {
-				mTimer.cancel();
-				mTimer.purge();
-			} catch (Exception e) {
-			}
-			mTimer = null;
-		}
 		mPreviousLocation=null;
 	}	
 	private void startTimerHeartbeat(long trigger, long interval) {
-		getTimer().schedule(new TimerTask() {
-			public void run() {
-				try {
-					Intent broadcastIntent = new Intent();
-			        broadcastIntent.setAction(GlobalStaticValues.HEARTBEAT_NOTIFICATION);
-			        // Broadcast whichever result occurred
-			        LocalBroadcastManager.getInstance(TimerService.this).sendBroadcast(broadcastIntent);
-
-					Location location=mLocationClient.getLastLocation();
-					doS(location);
-				} catch (Exception ee) {
-					
-				}
-			}
-		}, trigger, interval);
 	}
 
-	private Timer getTimer() {
-		if (mTimer == null) {
-			mTimer = new Timer("KidMinderHeartbeat");
-		}
-		return mTimer;
-	}
 
 	public TimerService() {		
 	}
@@ -175,12 +207,7 @@ public class TimerService extends Service implements GooglePlayServicesClient.Co
 
 	@Override
 	public void onConnected(Bundle arg0) {
-		Location location=mLocationClient.getLastLocation();
-		if(location!=null) {
-			doS(location);
-		}
-		int frequency=mSettingsManager.getHeartbeatFrequency();
-		startTimerHeartbeat(1000*frequency,1000*frequency);
+		mLocationClient.requestLocationUpdates(mLocationRequest, this);
 	}
 
 
